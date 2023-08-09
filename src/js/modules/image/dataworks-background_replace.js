dff7724c3f19538d0166d08604fd6ce16b260d79
corey alix <coreyalix@outlook.com>
2023-08-09 14:40:50 -0400

    Modify the order the colors are presented to the user...custom colors render on the background replace dialog directly.

--- src/js/modules/image/dataworks-background_replace.js
+++ src/js/modules/image/dataworks-background_replace.js
@@ -3,12 +3,12 @@ import app from './../../app.js';
 import config from '../../config.js';
 import Dialog_class from '../../libs/popup.js';
 import Base_layers_class from '../../core/base-layers.js';
-import { data } from 'jquery';
 
 var instance = null;
 
 const COLORS_TO_REMEMBER = 3;
 const DEFAULT_COLOR = '#757575';
+const ALTERNATIVE_COLORS = ['#72d6ef'];
 
 function injectCustomColorsIntoColorPicker(colorPicker) {
     const datalist = document.createElement('datalist');
@@ -27,9 +27,9 @@ function injectCustomColorsIntoColorPicker(colorPicker) {
             const theColor = colorPicker.value;
             const colors = readSetting('CUSTOM_COLORS', []);
             if (!colors.includes(theColor)) {
-                colors.unshift(theColor);
+                colors.push(theColor);
                 while (colors.length > COLORS_TO_REMEMBER) {
-                    const colorToRemove = colors.pop();
+                    const colorToRemove = colors.shift();
                     if (colorToRemove) {
                         log(`Remove color ${colorToRemove}`);
                         const optionToRemove = datalist.querySelector(`option[value="${colorToRemove}"]`);
@@ -40,8 +40,8 @@ function injectCustomColorsIntoColorPicker(colorPicker) {
                 }
                 writeSetting('CUSTOM_COLORS', colors);
                 const option = document.createElement('option');
-                option.innerText = theColor;
-                datalist.insertBefore(option, datalist.firstChild);
+                option.value = theColor;
+                datalist.append(option);
             }
         },
         false,
@@ -66,6 +66,12 @@ function rgbToHex(rgb) {
     const hexG = parseInt(g).toString(16);
     const hexB = parseInt(b).toString(16);
     return `#${hexR}${hexG}${hexB}`;
+}
+
+function getCustomColors() {
+    const colors = [...ALTERNATIVE_COLORS, ...readSetting('CUSTOM_COLORS', [])];
+    const distinct = [...new Set(colors)];
+    return distinct;
 }
 
 function getInitialColorForColorPicker() {
@@ -106,7 +112,6 @@ class Effects_backgroundReplace_class {
                 if (typeof swal == 'undefined') {
                     warn('Swal is not defined.');
                 }
-                const NewImage = document.getElementById('backgroundReplaceImageHolder');
 
                 const $label = $('<label>').text('Select a background color: ');
 
@@ -119,11 +124,13 @@ class Effects_backgroundReplace_class {
                         gap: 0.25rem;
                         flex: 1;
                     }
-                    .flex > * {
-                        width: 10rem;
+                    .flex > button {
+                        width: 6rem;
                         height: 3rem;
                     }
                     .flex > label {
+                        width: 10rem;
+                        height: 3rem;
                         no-wrap: true;
                     }
                     .flex input.color-picker {
@@ -142,9 +149,13 @@ class Effects_backgroundReplace_class {
                 </style>
                 <div class="flex">
                     <button class="btn btn-md BackgroundReplaceColorButton selected" type="button" style="background-color:${DEFAULT_COLOR}"></button>
-                    <button class="btn btn-md BackgroundReplaceColorButton selected" type="button" style="background-color:#72d6ef"></button>
-                    <button class="btn btn-md BackgroundReplaceColorButton selected color-picker-target" type="button" style="background-color:${getInitialColorForColorPicker()}"></button>
-                    <label class="color-picker" title="Open a color picker to select a custom color.">Pick a color <input class="color-picker" type="color" value="${getInitialColorForColorPicker()}"/></label>
+                    ${getCustomColors()
+                        .map(
+                            (color) =>
+                                `<button class="btn btn-md BackgroundReplaceColorButton" type="button" style="background-color:${color}"></button>`,
+                        )
+                        .join('')}
+                    <label class="color-picker" title="Open a color picker to select a custom color.">Add a color <input class="color-picker" type="color" value="${getInitialColorForColorPicker()}"/></label>
                 </div>
                 <label>Auto Replace? <input class="auto-replace" type="checkbox" checked/></label>
                 `;
@@ -155,33 +166,30 @@ class Effects_backgroundReplace_class {
                 const target = document.querySelector('[data-id="params_content"]');
                 $(target).append($label).append($div);
 
-                const _canvas = this.Base_layers.convert_layer_to_canvas(null, true);
-
                 $('.BackgroundReplaceColorButton').on('click', (e) => {
-                    const target = e.target;
-                    $('.BackgroundReplaceColorButton').removeClass('selected');
-                    $(target).addClass('selected');
-                    const backgroundColor = $(target).css('background-color');
-                    // convert rgb to hex
-                    const hexColor = rgbToHex(backgroundColor);
-                    log('hexColor', hexColor);
-                    GetNewReplacement(_canvas, hexColor);
+                    replaceBackground(e.target, { canvas_preview, w, h });
                 });
 
-                const colorPickerTarget = target.querySelector('.color-picker-target');
-                if (colorPickerTarget) {
-                    const colorPicker = target.querySelector('input.color-picker');
-                    if (colorPicker) {
-                        colorPicker.addEventListener(
-                            'input',
-                            () => {
-                                const theColor = colorPicker.value;
-                                $(colorPickerTarget).css('background-color', theColor);
-                            },
-                            false,
-                        );
-                        injectCustomColorsIntoColorPicker(colorPicker);
-                    }
+                const colorPicker = target.querySelector('input.color-picker');
+                if (colorPicker) {
+                    colorPicker.addEventListener(
+                        'change',
+                        () => {
+                            const theColor = colorPicker.value;
+                            const button = $.parseHTML(
+                                `<button class="btn btn-md BackgroundReplaceColorButton" type="button" style="background-color:${theColor}"></button>`,
+                            )[0];
+                            button.addEventListener('click', (e) =>
+                                replaceBackground(e.target, { canvas_preview, w, h }),
+                            );
+                            const flex = target.querySelector('.flex');
+                            // insert before the label
+                            const colorPickerLabel = flex.querySelector('label.color-picker');
+                            flex.insertBefore(button, colorPickerLabel);
+                        },
+                        false,
+                    );
+                    injectCustomColorsIntoColorPicker(colorPicker);
                 }
 
                 const autoReplace = target.querySelector('.auto-replace');
@@ -199,67 +207,7 @@ class Effects_backgroundReplace_class {
 
                 if (readSetting('AUTO_REPLACE_BACKGROUND', true)) {
                     // Get Default Replacement with Gray Background
-                    GetNewReplacement(_canvas, DEFAULT_COLOR);
-                }
-
-                function GetNewReplacement(canvas, colorInput) {
-                    if (typeof Swal == 'undefined') {
-                        warn('Swal is not defined.');
-                        return;
-                    }
-                    Swal.fire({
-                        title: 'Processing...',
-                        text: 'Please wait',
-                        allowOutsideClick: false,
-                        didOpen: () => {
-                            Swal.showLoading();
-                        },
-                    });
-
-                    var dataURL = canvas.toDataURL('image/jpeg');
-
-                    if (dataURL != null) {
-                        $.ajax({
-                            type: 'POST',
-                            url: '../DWPService.asmx/GetBackgroundReplaceImageV2',
-                            data: {
-                                imgBase64: dataURL,
-                                backgroundcolor: colorInput,
-                            },
-                            dataType: 'text',
-                            success: function (data, status) {
-                                // Remove XML Tags from response
-                                data = data.replace(/<.*>/gm, '');
-
-                                NewImage.src = data;
-                                NewImage.onload = function () {
-                                    var canvas = document.createElement('canvas');
-                                    var context = canvas.getContext('2d');
-                                    canvas.width = NewImage.width;
-                                    canvas.height = NewImage.height;
-
-                                    context.drawImage(NewImage, 0, 0, w, h);
-                                    var myData = context.getImageData(0, 0, NewImage.width, NewImage.height);
-
-                                    canvas_preview.putImageData(myData, 0, 0);
-
-                                    Swal.close();
-                                };
-                            },
-                            error: function (e) {
-                                Swal.close();
-
-                                reportError('Error Retrieving Image.');
-                                return;
-                            },
-                        });
-                    } else {
-                        // Error
-                        Swal.close();
-
-                        reportError('Error Sending Image.');
-                        return;
-                    }
+                    GetNewReplacement(DEFAULT_COLOR, { canvas_preview, w, h });
                 }
             },
             params: [],
@@ -298,3 +246,84 @@ class Effects_backgroundReplace_class {
 }
 
 export default Effects_backgroundReplace_class;
+
+function replaceBackground(target, options) {
+    $('.BackgroundReplaceColorButton').removeClass('selected');
+    $(target).addClass('selected');
+    const backgroundColor = $(target).css('background-color');
+    // convert rgb to hex
+    const hexColor = rgbToHex(backgroundColor);
+    log('hexColor', hexColor);
+
+    GetNewReplacement(hexColor, options);
+}
+
+function GetNewReplacement(colorInput, options) {
+    const { canvas_preview, w, h } = options;
+    const baseLayers = new Base_layers_class();
+    const canvas = baseLayers.convert_layer_to_canvas(null, true);
+
+    if (typeof Swal == 'undefined') {
+        warn('Swal is not defined.');
+        return;
+    }
+    Swal.fire({
+        title: 'Processing...',
+        text: 'Please wait',
+        allowOutsideClick: false,
+        didOpen: () => {
+            Swal.showLoading();
+        },
+    });
+
+    const NewImage = document.getElementById('backgroundReplaceImageHolder');
+    if (!NewImage) {
+        warn("'#backgroundReplaceImageHolder' is not defined.");
+        return;
+    }
+
+    var dataURL = canvas.toDataURL('image/jpeg');
+
+    if (dataURL != null) {
+        $.ajax({
+            type: 'POST',
+            url: '../DWPService.asmx/GetBackgroundReplaceImageV2',
+            data: {
+                imgBase64: dataURL,
+                backgroundcolor: colorInput,
+            },
+            dataType: 'text',
+            success: function (data, status) {
+                // Remove XML Tags from response
+                data = data.replace(/<.*>/gm, '');
+
+                NewImage.src = data;
+                NewImage.onload = function () {
+                    var canvas = document.createElement('canvas');
+                    var context = canvas.getContext('2d');
+                    canvas.width = NewImage.width;
+                    canvas.height = NewImage.height;
+
+                    context.drawImage(NewImage, 0, 0, w, h);
+                    var myData = context.getImageData(0, 0, NewImage.width, NewImage.height);
+
+                    canvas_preview.putImageData(myData, 0, 0);
+
+                    Swal.close();
+                };
+            },
+            error: function (e) {
+                Swal.close();
+
+                reportError('Error Retrieving Image.');
+                return;
+            },
+        });
+    } else {
+        // Error
+        Swal.close();
+
+        reportError('Error Sending Image.');
+        return;
+    }
+}
