d4107acbe5e51e2b0e705cc1d6f56e488044f1f7
corey alix <coreyalix@outlook.com>
2023-09-07 09:02:07 -0400

    If active tool is 'lasso' set it to 'select'

--- src/js/main.js
+++ src/js/main.js
@@ -66,6 +66,12 @@ window.addEventListener(
 
         // prevent prompting user when navigating away
         app.GUI.Tools_settings.save_setting('exit_confirm', false);
+        const activeTool = app.GUI.GUI_tools.Helper.getCookie('active_tool');
+        switch (activeTool) {
+            case 'lasso':
+                app.GUI.GUI_tools.Helper.setCookie('active_tool', 'select');
+                break;
+        }
 
         // Render all
         GUI.init();
